1957f4a7d7ae3724fb2a9025e30c3c7e6ad08f1e
Jeremy <jeremy.douchamps@emerya.fr>
2021-08-16 12:09:14 +0700

    First commit

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueFbCustomerChat from 'vue-fb-customer-chat'
-
-Vue.use(VueFbCustomerChat, {
-  page_id: 106606010809015, //  change 'null' to your Facebook Page ID,
-  theme_color: '#2c5282', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
-})
+// import Vue from 'vue'
+// import VueFbCustomerChat from 'vue-fb-customer-chat'
+//
+// Vue.use(VueFbCustomerChat, {
+//   page_id: 106606010809015, //  change 'null' to your Facebook Page ID,
+//   theme_color: '#2c5282', // theme color in HEX
+//   locale: 'en_US', // default 'en_US'
+// })
